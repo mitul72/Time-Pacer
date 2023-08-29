cd7abd6c03425644165f1009939524920639bfe4
mitul72 <mituldhawan2@gmail.com>
2023-08-29 08:51:05 -0600

    Added: headers for schedule table

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,25 +1,29 @@
 // import {useState} from 'react'
-import Tasks from './resources/Tasks'
-import { Button } from 'react-bootstrap'
-import {Link} from 'react-router-dom';
+import Tasks from "./resources/Tasks";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   const zeroTask = true;
   // const [zeroTask, setzeroTask] = useState(true)
   return (
-    <div className='Home'>
+    <div className="Home">
       <h1>TIME PACER</h1>
       <section>
-          <h2>Tasks</h2>
-          <div className="tasks">
-            {zeroTask ? <div className='daily-text'>
-              <span id='daily-text'>Daily tasks show up here</span>
+        <h2>Tasks</h2>
+        <div className="tasks">
+          {zeroTask ? (
+            <div className="daily-text">
+              <span id="daily-text">Daily tasks show up here</span>
               <Link to="build">
-              <Button className='button'>Add Task</Button>
+                <Button className="button">Add Task</Button>
               </Link>
-              </div>:<Tasks/>}
-          </div>
+            </div>
+          ) : (
+            <Tasks />
+          )}
+        </div>
       </section>
     </div>
-  )
+  );
 }
--- src/components/resources/Table.tsx
+++ src/components/resources/Table.tsx
@@ -1,19 +1,42 @@
 import React from "react";
 
 export default function Table() {
+  let x = 60; //minutes interval
+  let times = []; // time array
+  let tt = 0; // start time
+  let ap = ["AM", "PM"]; // AM-PM
+
+  //loop to increment the time and push results in array
+  for (let i = 0; tt < 24 * 60; i++) {
+    let hh = Math.floor(tt / 60); // getting hours of day in 0-24 format
+    let mm = tt % 60; // getting minutes of the hour in 0-55 format
+    let hh12 = hh % 12;
+    if (hh12 === 0) {
+      hh12 = 12;
+    }
+    times[i] =
+      ("0" + hh12).slice(-2) +
+      ":" +
+      ("0" + mm).slice(-2) +
+      ap[Math.floor(hh / 12)]; // pushing data in array in [00:00 - 12:00 AM/PM format]
+    tt = tt + x;
+  }
+
   return (
     <table>
       <thead>
         <tr>
-          <th>header1</th>
-          <th>header2</th>
-          <th>header3</th>
+          <th>Time Slot</th>
+          <th>Task</th>
+          <th>Priority</th>
         </tr>
       </thead>
       <tbody>
         <tr>
-          <td>text1.1</td>
-          <td>text1.2</td>
+          <td>{times[0]}</td>
+          <td>
+            <input type="text" name="" id="" />
+          </td>
           <td>text1.3</td>
         </tr>
         <tr>
